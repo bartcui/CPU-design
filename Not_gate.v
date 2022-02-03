ce1c0f2af3af7120c93445e0cb3dcd5a12c71765
Bart Cui <60012853+bartcui@users.noreply.github.com>
2022-02-03 15:06:16 -0500

    Add files via upload

---
+++ Not_gate.v
@@ -0,0 +1,9 @@
+module NOT_gate(input wire [31:0] A, output wire [31:0] Q);
+
+	genvar i;
+	generate
+		for (i=0; i<32, i=i+1) begin : loop
+			assign Q[i] = !A[i];
+		end
+	end generate
+end module
