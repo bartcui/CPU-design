0924f0544d6b5febed470f8b07a598f81dbc8453
Bart Cui <60012853+bartcui@users.noreply.github.com>
2022-02-03 17:03:24 -0500

    Add files via upload

--- AND_gate.v
+++ AND_gate.v
@@ -1,6 +1,5 @@
 module AND_gate(
-	input wire [31:0] A,
-	input wire [31:0] B,
+	input wire [31:0] A,B,
 	output wire [31:0] Y);
 
 	genvar i;
@@ -8,5 +7,5 @@ module AND_gate(
 		for (i=0; i<32, i=i+1) begin : loop
 			assign Y[i] = ((A[i])&(B[i]));
 		end
-	end generate
-end module
+	endgenerate
+endmodule
--- Not_gate.v
+++ Not_gate.v
@@ -5,5 +5,5 @@ module NOT_gate(input wire [31:0] A, output wire [31:0] Q);
 		for (i=0; i<32, i=i+1) begin : loop
 			assign Q[i] = !A[i];
 		end
-	end generate
-end module
+	endgenerate
+endmodule
