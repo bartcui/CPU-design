0ecf0244a57619e9e11451e7d33353018e41824d
aksabby <60979552+aksabby@users.noreply.github.com>
2022-02-08 15:21:45 -0500

    Fixed neg gate

--- NEG_gate.v
+++ NEG_gate.v
@@ -1,10 +1,10 @@
 module NEG_gate(
-	input wire [31:0] A,
-	output wire [31:0] Q);
+	input [31:0] A,
+	output [31:0] Q);
 	
 	wire [31:0] i; 
 	wire out;
-	NOT_gate not_gate(.A(A),.Q(i));
-	Add add_op(.c_out(out),.sum(Q), .A(i), .B(32'd1), .c_in(1'd0) );
+	NOT_gate not_gate(A, i);
+	Add add_op(Q, out, i, 32'd1, 0);
 	
 endmodule
