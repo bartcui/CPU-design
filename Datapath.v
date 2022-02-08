7ece18491fae1dc817ed2eef8124f4d0fdbe69fc
aksabby <60979552+aksabby@users.noreply.github.com>
2022-02-08 12:33:26 -0500

    Updated data path with more operation ALU logic
    datapath still not working

--- Datapath.v
+++ Datapath.v
@@ -3,10 +3,10 @@
 module Datapath(
 	output reg [63:0] out,
 	input PCout, Zhiout, Zlowout, MDRout, R2out, R4out, HIout, LOout,
-   input MARin, Zin, PCin, MDRin, IRin, Yin, HIin, LOin,    
-   input IncPC, Read, R5in, R2in, R4in,
-   input Clock, Clear, 
-   input [31:0] Mdatain,
+   	input MARin, Zin, PCin, MDRin, IRin, Yin, HIin, LOin,    
+   	input IncPC, Read, R5in, R2in, R4in,
+   	input Clock, Clear, 
+   	input [31:0] Mdatain,
 	input AND, OR, ADD, SUB, MUL, DIV, SHR, SHL, ROR, ROL, NEG, NOT
 );
 	reg [31:0] encIn;
@@ -18,8 +18,26 @@ module Datapath(
 	encoder_32_5 busEnc(s, encIn);
 	
 	wire [31:0] busOut;
+	
 	reg [63:0] Z;
-	wire [63:0] ZAdd;
+	wire [32:0] ZAdd, ZSub;
+	wire [63:0] ZMult;
+	wire [31:0] ZNeg, ZNot;
+	
+	always @(*) begin
+		if(ADD)
+			Z <= ZAdd;
+		else if(SUB)
+			Z <= ZSub;
+		else if(MUL)
+			Z <= ZMult;
+		else if(NEG)
+			Z <= ZNeg;
+		else if(NOT)
+			Z <= ZNot;
+		//Have other ALU operations here
+		out[31:0] <= MDRo;
+	end
 
 	wire [31:0] R2, R4, R5, PC, IR, ZHi, ZLo, Y, MAR, MDRo, HI, LO;
 	genReg R2ff(R2, busOut, R2in, Clear, Clock);
@@ -37,14 +55,11 @@ module Datapath(
 	
 	//Add the other operations needed for the datapath
 	Add add(ZAdd[31:0], ZAdd[32], Y, busOut, 0);
+	Sub sub(ZSub[31:0], ZSub[32], Y, busOut, 0);
+	Mult_booth_bit mult(ZMult[31:0], ZMult[63:32], Y, busOut);
+	NEG_gate neg(busOut, ZNeg);
+	NOT_gate nott(busOut, ZNot);
 	
 	busmux BUS(busOut, s, 32'b0, 32'b0, R2, 32'b0, R4, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-					32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, ZHi, ZLo, PC, MDRo);
-	
-	always @(*) begin
-		if(ADD)
-			Z <= ZAdd;
-		//Have other ALU operations here
-		out[31:0] <= MDRo;
-	end
+					32'b0, 32'b0, 32'b0, 32'b0, HI, LO, ZHi, ZLo, PC, MDRo);
 endmodule
